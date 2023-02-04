c62c36ee784300969fc516b9c7d4fd7e0869e13b
Alon <Alonsouter@gmail.com>
2023-02-04 06:45:53 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/CaterpillerKiller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaterpillerKiller : MonoBehaviour
+{
+    [SerializeField]
+     EaterChicken ec;
+    //GameObject caterpiller;
+    [SerializeField]
+    EaterChicken waker;
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Character_Warm cw = collision.GetComponent<Character_Warm>();
+        if (!cw)
+            return;
+        //cw.enabled = false;
+        //cw.rb.isKinematic = true;
+
+        //call caterpiller
+        waker.WakeChicken();
+
+    }
+}
