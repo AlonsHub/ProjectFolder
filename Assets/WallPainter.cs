a82b7ccbb3ba1ab2188bcf5f28a0ac6041cd83aa
Alon <Alonsouter@gmail.com>
2023-02-04 17:22:34 +0200

    finitos

---
+++ Assets/WallPainter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPainter : MonoBehaviour
+{
+    [SerializeField]
+    Renderer rend;
+    [SerializeField]
+    float totalTime;
+
+    //void Start()
+    //{
+    //    mat = GetComponent<Renderer>().material;
+    //}
+    [SerializeField]
+    Animator anim;
+
+    public void StartShiftToOne()
+    {
+        anim.SetTrigger("END");
+        //StartCoroutine(nameof(GradualDraw));
+    }
+
+    IEnumerator GradualDraw()
+    {
+        float t = 0;
+        while (t <= totalTime)
+        {
+            t += Time.deltaTime;
+            
+
+            rend.material.SetFloat("Vector1", -(t));
+            yield return null;
+        }
+    }
+}
